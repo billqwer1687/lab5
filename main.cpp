4251189767a896e7f0a2a734ac8efb2d85b61989
billqwer1687 <billqwer1687@gmail.com>
2016-05-17 16:02:29 +0800

    GG

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
-#include "Hugeint.h"
-using namespace std;
+ #include "Hugeint.h"
+ using namespace std;
 
-int main()
-{
-	HugeInt n1( 7654321 );
-	HugeInt n2( 7891234 );
-	HugeInt n3( "99999999999999999999999999999" );
-	HugeInt n4( "1" );
-	HugeInt n5;
-
-	cout << "n1 is " << n1 << "\nn2 is " << n2
-		<< "\nn3 is " << n3 << "\nn4 is " << n4
-		<< "\nn5 is " << n5 << "\n\n";
-
-	n5 = n1 + n2;
-	cout << n1 << " + " << n2 << " = " << n5 << "\n\n";
-
-	cout << n3 << " + " << n4 << "\n= " << ( n3 + n4 ) << "\n\n";
-
-	n5 = n1 + 9;
-	cout << n1 << " + " << 9 << " = " << n5 << "\n\n";
-
-	n5 = n2 + "10000";
-	cout << n2 << " + " << "10000" << " = " << n5 << endl;
-} 
+ int main()
+	 {
+		 
+		 HugeInt n1( "314159265358979323846" );
+		 HugeInt n2( "28825252" );
+		 
+		
+		
+			 cout << n1 << " + " << n2 << "\n= " << ( n1 + n2 ) << "\n\n";
+		
+		 } 
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
+lab5:main.o Hugeint.o
+	g++ -o lab5 main.o Hugeint.o
 Hugeint.o:Hugeint.cpp Hugeint.h
 	g++ -c Hugeint.cpp
 main.o:main.cpp Hugeint.h
 	g++ -c main.cpp
-lab5:main.o Hugeint.o
-	g++ -o lab5 main.o Hugeint.o
 clean:
 	rm main.o Hugeint.o
